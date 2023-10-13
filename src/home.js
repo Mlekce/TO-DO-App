9a785395993028fbe3df962430c68beb64962c04
Srdjan <stojanac.srdjan@gmail.com>
2023-10-13 20:08:21 +0200

    creating class Topic

---
+++ src/home.js
@@ -0,0 +1,28 @@
+class Topic {
+  constructor(name, priority) {
+    this.name = name;
+    this.priority = priority;
+    this.taskList = [];
+  }
+
+  showAllTasks() {
+    if (this.taskList) {
+      for (let task of this.taskList) {
+        console.log(task);
+      }
+    }
+  }
+
+  addTask(taskName) {
+    this.taskList.push(taskName);
+    return true;
+  }
+
+  removeTask(taskName) {
+    let index = this.taskList.indexOf(taskName);
+    if (index) {
+      return this.taskList.splice(index, 1);
+    }
+    return false;
+  }
+}
